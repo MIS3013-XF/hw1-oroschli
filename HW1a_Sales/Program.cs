1ef00bb46437c12419c4dcd839d394acc1263355
Olivia Roschli <Olivia.D.Roschli-1@ou.edu>
2025-09-10 15:41:20 -0500

    Console Added.

--- HW1a_Sales/Program.cs
+++ HW1a_Sales/Program.cs
@@ -1,11 +1,12 @@
 ﻿// HW1a Sales Total
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Olivia Roschli
+// Did you seek help ? If yes, specify the helper or web link here: No
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,42 @@ namespace HW1a_Sales
     {
         static void Main(string[] args)
         {
+            string prodname;
+            double quantity;
+            double price;
+            double sub;
+            double tax;
+            const double taxamt = 0.085;
+            double total;
+
+            string quantityAsString;
+            string priceAsString;
+
+            Console.WriteLine("What is the product name of the item you are purchasing?");
+            prodname = Console.ReadLine();
+            Console.WriteLine($"How many {prodname}'s do you want to buy?");
+            quantityAsString = Console.ReadLine();
+            Console.WriteLine($"What is the price for each {prodname}?");
+            priceAsString = Console.ReadLine();
+
+            quantity = Convert.ToDouble(quantityAsString);
+            price = Convert.ToDouble(priceAsString);
+
+            sub = quantity * price;
+            Console.WriteLine("Your subtotal for your bill is $" + sub.ToString("N2") + ".");
+
+            tax = taxamt * sub;
+            Console.WriteLine("Your sales tax for your bill is $" + tax.ToString("N2") + ".");
+
+            total = tax + sub;
+            Console.WriteLine("Your total for your bill is $" + total.ToString("N2") + ".");
+
+            Console.ReadKey();
+
+
+
+
+
         }
     }
 }
--- HW1b_Grade/Program.cs
+++ HW1b_Grade/Program.cs
@@ -1,11 +1,12 @@
 ﻿// HW1b Grade
 
-// Your Name:
-// Did you seek help ? If yes, specify the helper or web link here: 
+// Your Name: Olivia Roschli
+// Did you seek help ? If yes, specify the helper or web link here: No
 
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.Versioning;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -15,6 +16,64 @@ namespace HW1b_Grade
     {
         static void Main(string[] args)
         {
+            string fname;
+            string lname;
+            double homework;
+            double participation;
+            double quiz;
+            double midterm;
+            double final;
+            double sum;
+
+            string homeworkAsString;
+            string participationAsString;
+            string finalAsString;
+            string quizAsString;
+            string midtermAsString;
+            string idAsString;
+
+            Console.WriteLine("What is your first name?");
+            fname = Console.ReadLine();
+            Console.WriteLine("What is your last name?");
+            lname = Console.ReadLine();
+            Console.WriteLine("What is your student id?");
+            idAsString = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for homeworks?");
+            homeworkAsString = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for participations?");
+            participationAsString = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for quizzes?");
+            quizAsString = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for midterm?");
+            midtermAsString = Console.ReadLine();
+            Console.WriteLine("What is your overall percentage grade for final?");
+            finalAsString = Console.ReadLine();
+
+            const double hweight = 0.20;
+            const double pweight = 0.15;
+            const double qweight = 0.00;
+            const double mweight = 0.15;
+            const double fweight = 0.25;
+
+            homework = Convert.ToDouble(homeworkAsString);
+            participation = Convert.ToDouble(participationAsString);
+            quiz = Convert.ToDouble(quizAsString);
+            midterm = Convert.ToDouble(midtermAsString);
+            final = Convert.ToDouble(finalAsString);
+
+            homework = homework * hweight;
+            participation = participation * pweight;
+            quiz = quiz * qweight;
+            midterm = midterm * mweight;
+            final = final * fweight;
+
+            sum = homework + participation + quiz + midterm + final;
+
+            Console.WriteLine($"{fname} {lname} ({idAsString}), your final grade is {sum}%.");
+
+            Console.ReadKey();
+
+            
         }
     }
 }
